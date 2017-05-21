a0a421c301c9b1e896132e406248efc934cbfa42
eddie <eddie.power@icloud.com>
2017-05-21 23:11:56 +1000

    Initial Commit, Have wired up the Models for the text editor app, these are just basic file logic and also some text display logic.

---
+++ WpfNotepad/Models/DocumentModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNotepad.Models
+{
+    public class DocumentModel : ObservableObject
+    {
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { OnPropertyChanged(ref _text, value); }
+        }
+
+        private string _filePath;
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                //onproperty will set the value for us rather then 2 steps
+                OnPropertyChanged(ref _filePath, value);
+            }
+        }
+
+        private string _fileName;
+        public string FileName
+        {
+            get { return _fileName; }
+            set
+            {
+                //onproperty will set the value for us rather then 2 steps
+                OnPropertyChanged(ref _fileName, value);
+            }
+        }
+
+        /*  This flag will be used to check the fileName and FilePath is empty
+         *   or has not been saved 
+         */
+        public bool isEmpty
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(FileName) || string.IsNullOrEmpty(FilePath))               
+                    return true;
+                
+
+                return false;
+            }
+        }
+   }
+}
---
+++ WpfNotepad/ObservableObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNotepad
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        /* Using a generic <T> not always the same 
+         * data type so using a generic.
+         * CallerMember will get the name of the calling member like method name
+         */    
+        public void OnPropertyChanged<T>(ref T property, T value, [CallerMemberName] string PropertyName = "")
+        {
+            property = value;
+            var handler = PropertyChanged;
+            if(handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+}
